c39f800c65cbc242895a85d2225bda74e69d1aaa
xtian7489 <xtian7489@gmail.com>
2023-12-20 12:01:20 -0300

    ocultar imagen

--- src/app/_components/landing/banner-pacto-landing.jsx
+++ src/app/_components/landing/banner-pacto-landing.jsx
@@ -4,10 +4,10 @@ export default function BannerPactoLanding({ description }) {
 
     return (<div className="banner-interciudad has-background-cream p-6" id="banner-interciudad">
         <h1 className="has-text-centered is-size-2"> Pacto Inter-Ciudad</h1>
-        <div className="has-text-centered py-5">
+        {/* <div className="has-text-centered py-5">
             <img src="/images/image-pacto-interciudad.png" alt="pacto inter-ciudad" />
 
-        </div>
+        </div> */}
         <div className="w-75 my-5 mx-auto" dangerouslySetInnerHTML={{ __html: description }}>
 
         </div>
