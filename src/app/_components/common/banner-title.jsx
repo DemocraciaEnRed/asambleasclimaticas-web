857ebe05ce924629f8a1d2e82392fe9f13b19697
xtian7489 <xtian7489@gmail.com>
2023-12-20 09:00:50 -0300

    cambios banner title

--- src/app/_components/common/banner-title.jsx
+++ src/app/_components/common/banner-title.jsx
@@ -1,14 +1,15 @@
 import Logo from "./logo"
 
 const BannerTitle = ({ urlAction, title, subtitle, image, actionClick, textButton }) =>
-  <div className='hero is-fullheight banner-title is-flex is-justify-content-center is-align-items-flex-start is-flex-direction-column'
+  <div className='hero is-fullheight banner-title is-flex is-justify-content-center is-align-items-flex-start is-flex-direction-column is-relative'
     style={{ backgroundImage: `url('${image}')` }}>
-    <div className="w-75">
+    <div className="is-overlay"></div>
+    <div className="w-75 content">
 
       <Logo color='#2B1B18' widthLogo='30%' />
       <div className="my-5">
-        <p className=' is-size-1 has-text-brown has-text-weight-bold is-uppercase '>{title}</p>
-        <p className="has-text-weight-light is-size-4 has-text-brown ">{subtitle}</p>
+        <p className=' title has-text-brown-dark  is-uppercase '>{title}</p>
+        <p className="has-text-weight-light is-size-4 has-text-brown-dark has-text-weight-bold ">{subtitle}</p>
       </div>
     </div>
     {textButton && <button onClick={actionClick} className="button is-white is-rounded px-6 is-uppercase">{textButton}</button>}
