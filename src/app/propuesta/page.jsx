7b88a829c8e530a8c6ed068745a72a421d22a51e
xtian7489 <xtian7489@gmail.com>
2023-12-21 19:15:38 -0300

    fix delete orphan page Propuestas

--- src/app/propuesta/page.jsx
+++
@@ -1,8 +0,0 @@
-import PropuestaComponent from "@/components/pacto/resumen"
-
-export default function Propuesta() {
-
-    return <div className="propuesta-wrapper ">
-        <PropuestaComponent />
-    </div>
-}
