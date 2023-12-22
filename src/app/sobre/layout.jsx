509c08d638ba5117281cdbf885392782f7863bb8
xtian7489 <xtian7489@gmail.com>
2023-12-22 08:09:55 -0300

    separa nav links de acerca de

--- src/app/sobre/layout.jsx
+++ src/app/sobre/layout.jsx
@@ -1,38 +1,15 @@
 
-import Link from "next/link";
+import AboutNavLinks from "@/components/acerca-de/about-nav-links";
 
-const links = [
-    {
-        'name': 'Acerca de',
-        'url': '/sobre'
-    },
-    {
-        'name': 'Cómo participar',
-        'url': '/sobre/como-participar'
-    },
-    {
-        'name': 'Contacto',
-        'url': '/sobre/contacto'
-    }
-]
 
 export default function Layout({children}) {
-    console.log(children);
+
 
     return (
         <div className="acerca-de-wrapper">
             <div className='banner-wrapper has-background-primary'></div>
             <div className='static-info-wrapper'>
-                <div className='static-info-nav'>
-                    {links.map((link, i) => (
-                        <Link
-                            className={`button-section ${link.value ? 'active' : ''}`}
-                            href={link.url}
-                            key={i}>
-                            {link.name}
-                        </Link>
-                    ))}
-                </div>
+                <AboutNavLinks/>
                 {children}
             </div>
         </div>
---
+++ src/components/acerca-de/about-nav-links.jsx
@@ -0,0 +1,32 @@
+'use client'
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+
+const links = [
+    {
+        'name': 'Acerca de',
+        'url': '/sobre'
+    },
+    {
+        'name': 'Cómo participar',
+        'url': '/sobre/como-participar'
+    },
+    {
+        'name': 'Contacto',
+        'url': '/sobre/contacto'
+    }
+]
+
+export default function AboutNavLinks(){
+    const pathname = usePathname()
+    return(<div className='static-info-nav'>
+    {links.map((link, i) => (
+        <Link
+            className={`button-section ${pathname === link.url ? 'has-text-primary' : ''}`}
+            href={link.url}
+            key={i}>
+            {link.name}
+        </Link>
+    ))}
+</div>)
+}
