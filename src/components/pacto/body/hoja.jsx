05afc49a11550e7dab4c837acd471197d177b6fd
xtian7489 <xtian7489@gmail.com>
2023-12-27 09:04:23 -0300

    agrega linea de tiempo

--- src/components/pacto/body/hoja.jsx
+++ src/components/pacto/body/hoja.jsx
@@ -1,30 +1,43 @@
+import { formatDate } from "@/utils/format"
 import { fetchProjectEvents } from "@/utils/get-data"
 
-export default async function HojaBody({project}) {
+export default async function HojaBody({ project }) {
     const events = await fetchProjectEvents(project._id)
     return <>
-        <div>
+        <div className="hoja-de-ruta-wrapper">
             <div className="my-4">
 
-            <h1 className=" is-size-3 has-text-primary mb-0">Hoja de Ruta de las asambleas</h1>
-            <span className="has-text-weight-light is-italic is-size-4 has-text-primary"> * Roteiro das Assembleias</span>
+                <h1 className=" is-size-3 has-text-primary mb-0">Hoja de Ruta de las asambleas</h1>
+                <span className="has-text-weight-light is-italic is-size-4 has-text-primary"> * Roteiro das Assembleias</span>
             </div>
             <p className="mb-0 has-text-weight-bold">Ordenado de mas reciente al mas antiguo</p>
             <span className="has-text-weight-light is-italic is-size-7"> *Ordenado do mais recente ao mais antigo</span>
 
             <div className="events">
-                {events.events.map(event => <div key={event._id} className="p-5 has-background-cream-light mb-3 w-75">
-                <h1 className=" is-size-5 mb-0">{event.title_es}</h1>
-            <span className="has-text-weight-light is-italic is-size-6"> * {event.title_pt}</span>
-            <div className="my-5">
+                {events.events.map(event => <div key={event._id} className="columns event">
+                    <div className="py-5 pl-5 column">
+                        <div className="date is-flex is-relative is-justify-content-space-between">
+                            <p className="pt-1">{formatDate(event.date, '-')}</p>
+                            <div className="point"></div>
+                        </div>
+                    </div>
+                    <div className=" column is-10">
 
-            <p className="mb-2">{event.text_es}</p>
-            <p className="has-text-weight-light is-italic is-size-6"> * {event.text_pt}</p>
-            </div>
+                        <div className="py-5 pl-0 has-background-cream-light mb-3 w-75">
+                            <h1 className=" is-size-5 mb-0">{event.title_es}</h1>
+                            <span className="has-text-weight-light is-italic is-size-6"> * {event.title_pt}</span>
+                            <div className="my-5">
 
-            </div>)}
+                                <p className="mb-2">{event.text_es}</p>
+                                <p className="has-text-weight-light is-italic is-size-6"> * {event.text_pt}</p>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                )}
             </div>
         </div>
-            {}
+
     </>
 }
--- src/utils/format.js
+++ src/utils/format.js
@@ -1,3 +1,3 @@
-export const formatDate = (date) => {
-    return (date.substring(0, 10).split('-').reverse().join('/'))
-  }
+export const formatDate = (date, toJoin = '/') => {
+  return (date.substring(0, 10).split('-').reverse().join(toJoin))
+}
