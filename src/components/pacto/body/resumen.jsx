375ddbfe619ffca52a4fbe0375c58136e96c64fd
xtian7489 <xtian7489@gmail.com>
2023-12-29 08:54:53 -0300

    agrega stages a resumen

--- src/components/pacto/body/resumen.jsx
+++ src/components/pacto/body/resumen.jsx
@@ -1,4 +1,5 @@
 'use client'
+import { stageCountryList } from "@/utils/data";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Remark } from "react-remark";
@@ -11,7 +12,7 @@ export default function ResumenBody({ project }) {
 
         <div className="columns">
             <div className="column is-9">
-                <iframe
+                {project.youtubeUrl && <iframe
                     width="100%"
                     height="480"
                     src={`https://www.youtube.com/embed/${project.youtubeUrl.split('?v=')[1].split('&list')[0]}`}
@@ -19,16 +20,28 @@ export default function ResumenBody({ project }) {
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowFullScreen
                     title="Embedded youtube"
-                />
-                <div className="content mt-6">
+                />}
+                <div className="content mt-6 is-size-7-touch">
                     <Remark>{project.about_es}</Remark>
 
                 </div>
 
-                <button className="contrib-button has-text-weight-bold has-text-white has-background-primary w-100 my-5 is-flex is-justify-content-space-between p-4 is-size-6">
+                <button className="contrib-button has-text-weight-bold has-text-white has-background-primary w-100 mt-5 is-flex is-justify-content-space-between p-4 is-size-6">
                     Contribuye dejando tus aportes en las máximas del Pacto Interciudad
                     <FontAwesomeIcon icon={faArrowRight} />
                 </button>
+
+                <div className="m-0 mt-6 is-flex is-flex-wrap-wrap is-justify-content-center stage-wrapper">
+                    {stageCountryList.map(country =>
+                        <div className={`stage is-flex is-align-items-center  my-3 pr-3 ${project.stage === country.code ? 'active' : ''}`}>
+                            <figure className="image is-48x48">
+                                <img className="" src={country.image} />
+                            </figure>
+                            <div className="name-country has-background-black is-flex-grow-1 has-text-centered has-text-white px-3 is-uppercase">
+                                {country.name}
+                            </div>
+                        </div>)}
+                </div>
 
             </div>
         </div>
