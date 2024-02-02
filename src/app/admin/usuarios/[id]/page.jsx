9538f8b95c28e179f44dc2297b042edb39dbf348
Guillermo Croppi <guillermocroppi@gmail.com>
2024-02-02 16:39:48 -0300

    Add UserInfoAdminPage and UserInfoForm component

--- src/app/admin/usuarios/[id]/page.jsx
+++ src/app/admin/usuarios/[id]/page.jsx
@@ -1,14 +1,16 @@
 "use client"
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
 import { useRouter, redirect } from "next/navigation"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useSelector } from "react-redux";
 import Link from 'next/link'
 import Image from 'next/image'
 import axiosServices from "@/utils/axios";
-import { faCheck, faDownload, faPenClip, faShield, faSync, faTimes, faUserEdit, faUserShield } from "@fortawesome/free-solid-svg-icons";
+import { faAngleDoubleRight, faCheck, faDownload, faExclamationTriangle, faPenClip, faShield, faSync, faTimes, faUserEdit, faUserShield } from "@fortawesome/free-solid-svg-icons";
 import Emoji from "@/components/common/emoji";
-import { faCheckCircle, faTimesCircle } from "@fortawesome/free-regular-svg-icons";
+import { faCheckCircle, faSave, faTimesCircle, faUser } from "@fortawesome/free-regular-svg-icons";
+import { setMessage } from "@/store/reducers/alert"
+import UserInfoForm from "@/components/admin/userInfoForm";
 
 export default function AdminUserInfoPage({params}) {
   // get the user from store
@@ -28,9 +30,11 @@ export default function AdminUserInfoPage({params}) {
 
   const userId = params.id
   const [userData, setUserData] = useState(null);
+  const [projectsTotal, setProjectsTotal] = useState(0)
   const [fetchFailed, setFetchFailed] = useState(false)
   const [isLoading, setIsLoading] = useState(true)
   const [isUpdating, setIsUpdating] = useState(false)
+  const userInfoRef = useRef(null)
 
   useEffect(() => {
     fetchData()
@@ -41,20 +45,74 @@ export default function AdminUserInfoPage({params}) {
       // fetch
       const promises = [
         axiosServices.get(`/admin/users/${userId}`),
+        axiosServices.get(`/projects?author=${userId}`)
       ]
 
-      const [userRes] = await Promise.all(promises)
+      const [userRes, projectRes] = await Promise.all(promises)
 
       // attach projects to array "projects"
       const user = userRes.data
+      const projectsTotal = projectRes.data.total
       setUserData(user)
+      setProjectsTotal(projectsTotal)
       setIsLoading(false)
+      setIsUpdating(false)
     } catch (err) {
       setFetchFailed(true)
       setIsLoading(false)
-      console.log(err)
+      setIsUpdating(false)
+      console.error(err)
     }
 
+  }
+
+  function getClassTagForRole (role) {
+    if(userData.role === role) {
+      return 'tag is-medium is-clickable is-primary'
+    }
+    return 'tag is-medium is-clickable'
+  
+  }
+
+  async function setUserRole(role) {
+    if(role === userData.role) return
+    try {
+      setIsUpdating(true)
+      const response = await axiosServices.put(`/admin/users/${userId}/role`, {role})
+      fetchData()
+      setMessage({
+        message: 'Rol de usuario actualizado', 
+        type:'success'
+      })
+
+    } catch (error) {
+      console.error(error)
+      setMessage({
+        message: 'Ha ocurrido un error al actualizar el rol del usuario', 
+        type:'danger'
+      })
+    }
+  }
+
+  async function saveUserData() {
+    const userInfo = userInfoRef.current.getUserInfo()
+    console.log(userInfo)
+    try {
+      setIsUpdating(true)
+      const response = await axiosServices.put(`/admin/users/${userId}`, userInfo)
+      fetchData()
+      setMessage({
+        message: 'Datos de usuario actualizados', 
+        type:'success'
+      })
+
+    } catch (error) {
+      console.error(error)
+      setMessage({
+        message: 'Ha ocurrido un error al actualizar los datos del usuario', 
+        type:'danger'
+      })
+    }
   }
 
   if(isLoading) {
@@ -71,97 +129,72 @@ export default function AdminUserInfoPage({params}) {
     </div>
   }
 
-  function getRoleTag (user) {
-    switch(user.role) {
-      case 'admin':
-        return <span className="has-text-info">
-          <FontAwesomeIcon icon={faShield} className="mr-1" /> Admin
-        </span>
-      case 'moderator':
-        return <span className="has-text-warning">
-          <FontAwesomeIcon icon={faUserShield} className="mr-1" /> Moderador
-        </span>
-      case 'author':
-        return <span className="has-text-success">
-          <FontAwesomeIcon icon={faPenClip} className="mr-1" /> Autor
-        </span>
-      default:
-        return <span className="has-text-grey">
-          Usuario
-          </span>
-    }
-  }
-
-  function getCountry (user) {
-    if(user.country) {
-      return (
-        <span className="">
-          <Emoji emoji={user.country.emoji} /> {user.country.name}
-        </span>
-      )
-    }
-    return 'Sin país'
-  }
-
-  function getLanguage (user) {
-    switch(user.lang) {
-      case 'es':
-        return 'Español'
-      case 'pt':
-        return 'Portugués'
-      default:
-        return '-Invalid-'
-    }
-  }
-
   return (
     <>
+      { isUpdating && <FontAwesomeIcon icon={faSync} spin className="has-text-primary" style={{position: 'absolute', right: '1rem', top: '1rem'}} /> }
       <h1 className="subtitle is-6 has-text-grey"><Link href="/admin/usuarios">Usuarios</Link> / Ver usuario</h1>
-        
-      <h1 className="title is-3 mb-3">{userData.name}</h1>
-      <hr />
-      <div className="">
-        <div className="mb-3">
-          <dt className="has-text-weight-bold">Acerca de mí</dt>
-          <dd className="is-italic">{userData.bio || '-'}</dd>
-        </div>
-        <div className="columns is-multiline">
-          <div className="column is-6">
-            <dt className="has-text-weight-bold">Nombre</dt>
-            <dd>{userData.name}</dd>
-          </div>
-          <div className="column is-6">
-            <dt className="has-text-weight-bold">Email</dt>
-            <dd><a className="has-text-info is-underlined" href={`mailto:${userData.email}`}>{userData.email}</a></dd>
-          </div>
-          <div className="column is-6">
-            <dt className="has-text-weight-bold">Lenguaje</dt>
-            <dd>{getLanguage(userData)}</dd>
-          </div>
-          <div className="column is-6">
-            <dt className="has-text-weight-bold">País</dt>
-            <dd>{getCountry(userData)}</dd>
-          </div>
-          <div className="column is-6">
-            <dt className="has-text-weight-bold">Rol asignado</dt>
-            <dd>{getRoleTag(userData)}</dd>
-          </div>
-          <div className="column is-6">
-            <dt className="has-text-weight-bold">Verificado</dt>
-            <dd>
-              {
-                userData.isVerified ? <span className="has-text-success">
-                  <FontAwesomeIcon icon={faCheckCircle} className="" /> Verificado
-                </span> : <span className="has-text-danger">
-                  <FontAwesomeIcon icon={faTimesCircle} className="" /> No verificado
-                </span>
-              }
-            </dd>
-          </div>
-          
-        </div>
-        
+      <h1 className="title is-3 mb-5">{userData.name}</h1>
+      <div className="tabs is-fullwidth mb-5">
+        <ul>
+          <li className="is-active"><a>Perfil & Rol</a></li>
+          {/* <li><a>Cambiar email</a></li>
+          <li><a>Cambiar contraseña</a></li> */}
+          {/* {
+            !userData.isVerified && <li><a>Verificar usuario</a></li>
+          }
+
+          {
+            (['admin', 'author']).includes(userData.role) && <li><a>Proyectos</a></li>
+          } */}
+        </ul>
       </div>
+
+      <UserInfoForm userInfo={userData} ref={userInfoRef} />
+      <div className="buttons is-right mt-4 mb-0">
+        <button className="button is-primary" onClick={saveUserData}><FontAwesomeIcon icon={faSave} />&nbsp;Guardar</button>
+      </div>
+      <hr />
+      <h3 className="title is-4 mb-1 has-text-weight"><FontAwesomeIcon icon={faAngleDoubleRight} /> Rol del usuario</h3>
+      <p className="mb-3">Haga clic para cambiar el rol del usuario.<br /><b>NOTA:</b> Considere que si el usuario es autor o admin, bajar su rol puede hacer que pierda acceso a ciertas funciones o acceso a sus propios contenidos.</p>
+      {/* <dd>{getRoleTag(userData)}</dd> */}
+      <div className="tags">
+        <span className={getClassTagForRole('admin')}
+          onClick={() => setUserRole('admin')}
+        >
+          <FontAwesomeIcon icon={faShield} />&nbsp;Admin</span>
+        <span className={getClassTagForRole('author')}
+          onClick={() => setUserRole('author')}
+        >
+          <FontAwesomeIcon icon={faPenClip} />&nbsp;Autor</span>
+        <span className={getClassTagForRole('moderator')}
+          onClick={() => setUserRole('moderator')}
+        >
+          <FontAwesomeIcon icon={faUserShield} />&nbsp;Moderador</span>
+        <span className={getClassTagForRole('user')}
+          onClick={() => setUserRole('user')}
+        >
+          <FontAwesomeIcon icon={faUser} />&nbsp;Usuario</span>
+      </div>
+      {
+        ([ 'admin', 'author' ].includes(userData.role)) && projectsTotal > 0 && (
+          <div className="notification is-warning py-3 px-4">
+            <FontAwesomeIcon icon={faExclamationTriangle} />&nbsp;El usuario tiene {projectsTotal} proyectos asociados. Considere que si el usuario es autor o admin, bajar su rol puede hacer que pierda acceso a sus propios contenidos. Evalue cambiar de propietario los proyectos antes de cambiar el rol.
+          </div>
+        )
+      }
+      <hr />
+      <h3 className="title is-4 mb-1 has-text-weight"><FontAwesomeIcon icon={faAngleDoubleRight} /> Usuario verificado</h3>
+      {
+        userData.isVerified ? (
+          <div className="notification is-success py-3 px-4">
+            <FontAwesomeIcon icon={faCheckCircle} />&nbsp;El usuario ha verificado su correo electrónico
+          </div>
+        ) : (
+          <div className="notification is-danger py-3 px-4">
+            <FontAwesomeIcon icon={faTimesCircle} />&nbsp;El usuario aún no ha verificado su correo electrónico
+          </div>
+        )
+      }
     </>
   )
 
