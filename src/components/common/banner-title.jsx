9ed89aa3044ca5f9cb97075bff4934e0a11f1d33
xtian7489 <xtian7489@email.com>
2024-03-19 07:49:02 -0300

    fix logo banner

--- src/components/common/banner-title.jsx
+++ src/components/common/banner-title.jsx
@@ -1,5 +1,6 @@
 import Link from "next/link"
 import ImageRandom from "./banner-image-random";
+import Image from "next/image";
 
 export default function BannerTitle({
     title,
@@ -12,25 +13,30 @@ export default function BannerTitle({
             <ImageRandom />
             <div className="is-overlay"></div>
             <div className="banner-content w-100 hero is-fullheight is-align-items-center is-justify-content-center">
-                <div className="content">
-                    <div className="my-5 has-text-centered">
-                        <h1 className="title has-text-centered has-text-white has-text-weight-bold is-uppercase mb-0">
-                            {title}
-                        </h1>
-                        <h2 className=" has-text-white is-size-2 is-size-6-touch is-italic">
-                            {subtitle}
-                        </h2>
-                    </div>
-                    <div className="actions mx-auto">
-                        {textButton && urlButton && (
-                            <Link
-                                href={urlButton}
-                                className="button is-white is-rounded px-6 is-uppercase w-100"
-                            >
-                                {textButton}
-                            </Link>
-                        )}
-                    </div>
+                <Image
+                    width={0}
+                    height={0}
+                    sizes="100vw"
+                    src="/images/logoSimple.svg"
+                    alt="logo resurgentes"
+                />
+                <div className="my-5 has-text-centered">
+                    <h1 className="title has-text-centered has-text-white has-text-weight-bold is-uppercase mb-0">
+                        {title}
+                    </h1>
+                    <h2 className=" has-text-white is-size-2 is-size-6-touch is-italic">
+                        {subtitle}
+                    </h2>
+                </div>
+                <div className="actions mx-auto">
+                    {textButton && urlButton && (
+                        <Link
+                            href={urlButton}
+                            className="button is-white is-rounded px-6 is-uppercase w-100"
+                        >
+                            {textButton}
+                        </Link>
+                    )}
                 </div>
             </div>
         </div>
