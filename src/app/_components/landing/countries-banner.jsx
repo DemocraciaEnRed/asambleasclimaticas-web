625b7b7d8ad0599bc53d25ab8cd0b9d45ce89c16
xtian7489 <xtian7489@email.com>
2024-03-21 08:09:54 -0300

    Align  card country

--- src/app/_components/landing/countries-banner.jsx
+++ src/app/_components/landing/countries-banner.jsx
@@ -8,18 +8,18 @@ export default function CountriesBanner({ countries, title }) {
         <h1 className="has-text-color-white has-text-centered is-size-2 has-text-weight-bold">{title}</h1>
         <div className="card-group is-flex is-justify-content-space-evenly is-flex-wrap-wrap my-4">
             {countries.map(country =>
-                <div className="card  my-5" key={country.code}>
+                <div className="card  my-5 is-flex is-flex-direction-column" key={country.code}>
                     <div className="image-card is-flex is-justify-content-center pb-4">
                         <figure className="image is-96x96">
                             <img className="" src={country.image} />
                         </figure>
                         {/* <div className="p-6" style={{backgroundImage:`url('${country.image}')`}}/> */}
                     </div>
-                    <div className="card-content py-0 has-text-centered">
+                    <div className="card-content py-0 has-text-centered is-flex-grow-1 is-flex is-flex-direction-column ">
                         <p className="title is-size-4 pb-4">
                             {country.name}
                         </p>
-                        <div className="content">
+                        <div className="content is-flex-grow-1 is-flex is-flex-direction-column  is-justify-content-center">
                             <ul>
                                 <li><strong>Ciudad: </strong>{country.city}</li>
                                 <li><strong>Fecha: </strong>{country.date}</li>
