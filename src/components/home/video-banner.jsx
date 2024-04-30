0eb3cc678fb1a5cb5e595da40124552642c9f8ac
xtian7489 <xtian7489@gmail.com>
2024-04-30 15:31:49 -0300

    change video pacto

--- src/components/home/video-banner.jsx
+++ src/components/home/video-banner.jsx
@@ -7,7 +7,7 @@ function VideoBanner() {
   return (
     <div className='banner-image has-background-black is-flex is-align-items-center'>
         <video controls>
-        <source src={`/video/video_banner_${language}.mp4`} type="video/mp4"/>
+        <source src={`/video/video_banner_${language}.webm`} type="video/mp4"/>
 
         </video>
 
