8a8527239deed2c846204ecef5605a9db5c44984
Guillermo Croppi <guillermocroppi@gmail.com>
2024-05-09 12:40:36 -0300

    refactor: update video banner component to support multiple languages

--- src/components/home/video-banner.jsx
+++ src/components/home/video-banner.jsx
@@ -3,11 +3,13 @@ import { useLanguage } from '@/context/lang-context';
 import React from 'react'
 
 function VideoBanner() {
-    const { language } = useLanguage();
+    let { language } = useLanguage();
+    // if language is pt, set language to pt, else set language to es
+    language = language === 'pt' ? 'pt' : 'es';
   return (
     <div className='banner-image has-background-black is-flex is-align-items-center'>
-        <video controls>
-        <source src={`/video/video_banner_${language}.webm`} type="video/mp4"/>
+        <video muted controls poster={`https://democraciaenred.nyc3.digitaloceanspaces.com/projects/asambleasclimaticas/video_banner_${language}.png`} loop>
+        <source src={`https://democraciaenred.nyc3.digitaloceanspaces.com/projects/asambleasclimaticas/video_banner_${language}.webm`} type="video/webm"/>
 
         </video>
 
