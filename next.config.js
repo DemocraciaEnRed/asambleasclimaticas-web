a9ec44c46110c212751180ba79750bcad001da81
Guillermo Croppi <guillermocroppi@gmail.com>
2024-07-22 17:19:20 -0300

    chore: Update Dockerfile to use node:20-alpine and update next to 14

--- next.config.js
+++ next.config.js
@@ -1,16 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+        unoptimized: true,
         remotePatterns: [
             {
                 protocol: 'https',
                 hostname: 'cdn-icons-png.flaticon.com'
             }
         ]
-    },
-    experimental: {
-        serverActions: true,
-    },
+    },  
 }
 
 module.exports = nextConfig
